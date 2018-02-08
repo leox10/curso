79876dabdaece93a16a820fd604848b71af5888c
Usuario <Usuario@DESKTOP-ML6LQM2>
2018-02-08 14:38:24 -0600

    Metodo para buscar especialidad por id

--- src/main/java/com/seguritech/hospital/controller/EspecialidadController.java
+++ src/main/java/com/seguritech/hospital/controller/EspecialidadController.java
@@ -6,9 +6,13 @@
 package com.seguritech.hospital.controller;
 
 import com.seguritech.hospital.domain.Especialidad;
+import com.seguritech.hospital.domain.Rol;
 import com.seguritech.hospital.repository.especialidadrepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +34,15 @@ public class EspecialidadController
        List<Especialidad> roles = especialidadRepository.findAll();
        System.out.println(roles.size());
        return roles;
+    }
+    
+     @GetMapping("/especialidad/{id}")
+    public ResponseEntity<Especialidad> getRol(@PathVariable("id") Long id) {
+        Especialidad esp = especialidadRepository.findOne(id);
+        if (esp == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(esp);
     }
     
 }
