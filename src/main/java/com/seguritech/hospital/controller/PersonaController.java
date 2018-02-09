cacd589d58c4fb3410fda6235cf5ce90dbf3a160
Usuario <Usuario@DESKTOP-ML6LQM2>
2018-02-09 15:25:43 -0600

    Se agrego la consulta medico y paciente

---
+++ src/main/java/com/seguritech/hospital/controller/PersonaController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.seguritech.hospital.controller;
+
+import com.seguritech.hospital.domain.Medico;
+import com.seguritech.hospital.domain.Persona;
+import com.seguritech.hospital.repository.Medicorepository;
+import com.seguritech.hospital.repository.PersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Usuario
+ */
+@RestController
+public class PersonaController {
+    @Autowired
+    private PersonaRepository personaRepository;
+    @Autowired
+    private Medicorepository MedicoRepository;
+    
+    
+   @GetMapping(value = "/persona")
+    public List<Persona> findAllPersonas() {
+        List<Persona> usuarios = personaRepository.findAll();
+        return usuarios;
+    }
+    
+     @GetMapping(value = "/medico")
+    public List<Medico> findAllMedicos() {
+        return MedicoRepository.findAll();
+    }
+    
+    
+}
